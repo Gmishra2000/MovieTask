46e19b3987e3b11134e1b6c2beb54c7219f33f4a
Gmishra2000 <mantumisara485@gmail.com>
2021-11-28 14:57:37 +0530

    insert data

--- src/TheMain.java
+++ src/TheMain.java
@@ -1,9 +1,39 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class TheMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			DbConnection.connect();
+//			DbConnection.connect();
+			insert("Moneyball","Brad Pitt","Kerris Dorsey",2011,"Benett Miller");
 	}
 
+	private static void insert(String movieName, String leadActor, String actressName, double yearOfRelease, String directorName) {
+		  Connection con = DbConnection.connect();
+		  PreparedStatement ps = null; 
+		  try {
+		    String sql = "INSERT INTO movies(movieName, leadActor, actressName, yearOfRelease,directorName) VALUES(?,?,?,?,?) ";
+		    ps = con.prepareStatement(sql);
+		    ps.setString(1, movieName);
+		    ps.setString(2, leadActor);
+		    ps.setString(3, actressName);
+		    ps.setDouble(4, yearOfRelease);
+		    ps.setString(5,directorName);
+		    ps.execute();
+		    System.out.println("Data has been inserted!");
+		  } catch(SQLException e) {
+		    System.out.println(e.toString());
+		    // always remember to close database connections
+		  } finally {
+		    try{
+		      ps.close();
+		      con.close();
+		    } catch(SQLException e) {
+		      System.out.println(e.toString());
+		    }
+		    
+		  }
+		}
 }
