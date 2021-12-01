8075b4d2ed9e9c9674769217eda923ca126aff73
Gmishra2000 <mantumisara485@gmail.com>
2021-12-01 14:37:29 +0530

    read all data

--- src/TheMain.java
+++ src/TheMain.java
@@ -1,14 +1,16 @@
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 public class TheMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 //		DbConnection.connect();
-//		insert("Benson", "Karue", "benson@test.com",2011, "myPassword");
-		insert(" The Lord of the Rings","Elijah Wood","Angela Bassett",2003,"Peter Jackson");
+
+//		insert(" The Lord of the Rings","Elijah Wood","Angela Bassett",2003,"Peter Jackson");
+		readAllData();
 	}
 	
 	private static void insert(String movieName, String leadActor, String actressName,int yearOfRelease, String directorName) {
@@ -38,4 +40,44 @@ public class TheMain {
 		  }
 		}
 
+	
+	private static void readAllData() {
+	    Connection con = DbConnection.connect(); 
+	    PreparedStatement ps = null; 
+	    ResultSet rs = null; 
+	    
+	    try {
+	      String sql = "SELECT * FROM movies";
+	      ps = con.prepareStatement(sql); 
+	      rs = ps.executeQuery();
+	      System.out.println("ALL TOP MOVIES\n");
+	      while(rs.next()) {
+	        String movieName = rs.getString("movieName"); 
+	        String leadActor = rs.getString("leadActor"); 
+	        String actressName = rs.getString("actressName"); 
+	        int yearOfRelease = rs.getInt("yearOfRelease"); 
+	        String directorName = rs.getString("directorName");
+	        
+	        
+	        System.out.println("Movie Name: "+movieName);
+	        System.out.println("Lead Actor: "+leadActor);
+	        System.out.println("Actress Name: "+actressName);
+	        System.out.println("Year Of Release: "+yearOfRelease);
+	        System.out.println("Director Name: " +directorName+"\n\n");
+	        
+	      }
+	    } catch(SQLException e) {
+	      System.out.println(e.toString());
+	    } finally {
+	      try {
+	        rs.close();
+	        ps.close();
+	        con.close(); 
+	      } catch(SQLException e) {
+	        System.out.println(e.toString());
+	      }
+	    }
+	    
+	    
+	  }
 }
